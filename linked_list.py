41f944b21f78e99bf673f67682f9c732d4635410
justinwfu <justinwfu@gmail.com>
2020-06-22 19:13:13 -0700

    simplify and correct bugs

--- linked_list.py
+++ linked_list.py
@@ -7,83 +7,37 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        self.previous = None
-
-    def __str__(self):
-        return f'{self.value}'
-
-    def __repr__(self):
-        return f'{self.value}'
-
-
-print(f'testing Node object')
-print(f'raw testing {Node(5)}')
-n = Node(5)
-print(f'testing assigned node {n}')
 
 
 class LinkedList:
     def __init__(self):
-        self.current = None
         self.first = None
         self.last = None
-
-    def __str__(self):
-        return f'Current: {self.current} Previous: {self.current.previous}'\
-               f'Next: {self.current.next}'
-
-    def __repr__(self):
-        return f'{self.next}\t{self.previous}\t{self.current}'
+        self.current = None
 
     def append(self, value):
-        if self.current is None:
-            self.current = Node(value)
-            print(f'current = {self.current}')
-            self.first = self.current
-            # ahhhhhhh make sure assign that *same* node object
-            # dont instantiate a new one
-            # self.first = Node(value)
+        if self.current == None:
+            self.first = Node(value)
+            self.last = self.first
+            self.current = self.first
 
-            print(f'first = {self.current}')
-            # self.last = Node(value)
         else:
-            self.current.next = Node(value)
-            print(f'current.next = {self.current.next}')
-            self.current.next.previous = self.current
-            print(f'current.next.previous = {self.current.next.previous}')
-            self.current = self.current.next
-            print(f'current = {self.current}')
-
-            # self.last = Node(value)
+            self.last.next = Node(value)
+            self.last = self.last.next
 
     def prepend(self, value):
-        if self.current is None:
-            self.current = Node(value)
-        else:
-            self.first.previous = Node(value)
-            self.current = Node(value)
-            self.current.next = self.first
-            self.first = self.current
+            first_node = Node(value)
+            first_node.next = self.first
+            self.first = first_node
+            self.current = self.first
 
 
-ll = LinkedList()
-# print('linkedlist representation')
-# print(ll)
+# test cases
+# 1. append 
+# 2. prepend
+# 3. append and append
+# 4. append and prepend
+# 5. prepend and append
+# 6. append and prepend and append
+# 7. prepend and append and prepent
 
-ll.append(1)
-# self.current was None
-# self.current == Node(1)
-# self.first == Node(1)
-ll.append(2)
-# self.current was Node(1)
-# Node(1).next == Node(2)
-# Node(2).previous == Node(1)
-# self.current == Node(2)
-
-ll.prepend(0)
-
-print('testing iteration')
-while ll.current is not None:
-    print(f'current node value is {ll.current}')
-    # print(f'next node value is {ll.current.next}')
-    ll.current = ll.current.next
