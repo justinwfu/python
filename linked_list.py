b80beaf292f0798581718c657c8ddbdd8b9eebea
justinwfu <justinwfu@gmail.com>
2020-06-22 20:10:26 -0700

    none s/==/is

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,5 @@
-# Linked List
-# Collection of nodes fast to insert O(1) if indexing separate
-# Index O(n) is no current marker
+# linked_list.py
+# Sample Linked List Module
 
 
 class Node:
@@ -16,28 +15,21 @@ class LinkedList:
         self.current = None
 
     def append(self, value):
-        if self.current == None:
+        if self.current is None:
             self.first = Node(value)
             self.last = self.first
             self.current = self.first
-
         else:
             self.last.next = Node(value)
             self.last = self.last.next
 
     def prepend(self, value):
+        if self.current is None:
+            self.first = Node(value)
+            self.last = self.first
+            self.current = self.first
+        else:
             first_node = Node(value)
             first_node.next = self.first
             self.first = first_node
             self.current = self.first
-
-
-# test cases
-# 1. append 
-# 2. prepend
-# 3. append and append
-# 4. append and prepend
-# 5. prepend and append
-# 6. append and prepend and append
-# 7. prepend and append and prepent
-
